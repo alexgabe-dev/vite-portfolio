319e9eee912116937a83de16a67cdde297e9af6d
gbrsndr <172544124+gbrsndr@users.noreply.github.com>
2025-03-06 17:29:54 +0100

    set img size

--- src/App.tsx
+++ src/App.tsx
@@ -1410,7 +1410,9 @@ function App() {
                 <motion.img 
                   src="/vizitor-logo.png" 
                   alt="Vizitor Logo" 
-                  className="h-8 w-auto" 
+                  width={32}
+                  height={32}
+                  className="h-8 w-8 object-contain" 
                   whileHover={{ scale: 1.05 }}
                   transition={{ type: "spring", stiffness: 300 }}
                 />
